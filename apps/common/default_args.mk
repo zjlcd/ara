c9e3e0687eabe7bac2de2ad2f0e949be6e837a4a
Matteo Perotti <mperotti@iis.ee.ethz.ch>
2022-11-03 12:16:24 +0100

    [apps] Add lavaMD benchmark from RiVEC

--- apps/common/default_args.mk
+++ apps/common/default_args.mk
@@ -29,3 +29,5 @@ def_args_softmax     = "3 256"
 def_args_pathfinder  = "1 1024 64"
 # Batch_size, depth, height, width, n_boxes (in total), crop_h, crop_w
 def_args_roi_align   = "1 32 4 4 4 2 2"
+# n_boxes and alpha
+def_args_lavamd      = "2 0.5"
